e1cddbda0f6b02b56802537ddeaf7f93e289c75f
umer-iq <umeritsolutions@gmail.com>
2022-05-07 11:13:00 +0500

    .

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   //state ={progress:0}
   //setProgress =(progress) =>{
     //this.setState({progress:progress})
-  //}
+  // kj}
   render() {
     return (
       <div>
